8b1378d674d57272b9a8b47a5ff30f359efbd8b4
Chuck <ml28888@126.com>
2022-04-29 22:43:45 +0800

    done

--- src/views/IndexView/index.vue
+++ src/views/IndexView/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="index-container">
-    <div class="index-intro">
-      <p m2 p2>你好啊我是Chuck</p>
+    <div
+      class="index-intro"
+      transition
+      duration-200
+      hover:op40
+      hover:cursor-pointer
+    >
+      <p m2 p2>你好啊，我是Chuck</p>
+      <P m2 p2>我目前正在郑州念我的大学二年级。</P>
+      <p m2 p2>我热爱 Frontend 并且希望通过技术这个手段来给大家提供便利</p>
     </div>
   </div>
 </template>
